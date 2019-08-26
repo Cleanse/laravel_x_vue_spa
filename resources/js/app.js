3c2cc9883a75cc3c8b54dcc3dd09ece736ae0073
Paul E. Lovato <plovato@gmail.com>
2019-08-26 12:56:41 -0600

    Added momentjs to format dates via filters.

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,13 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import moment from 'moment';
+
+Vue.filter('formatDate', function(value) {
+    if (value) {
+        return moment(String(value)).format('MM/DD/YYYY');
+    }
+});
 
 new Vue({
     router,
