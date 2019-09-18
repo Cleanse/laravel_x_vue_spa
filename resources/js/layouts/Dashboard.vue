df5361437d2901bbcd44513f2234b0329a939550
Paul E. Lovato <plovato@gmail.com>
2019-09-18 17:31:51 -0600

    Re-added page-body and added a record icon.

--- resources/js/layouts/Dashboard.vue
+++ resources/js/layouts/Dashboard.vue
@@ -12,7 +12,7 @@
             </nav>
         </header>
 
-        <main>
+        <main class="page-body">
             <div class="container-fluid">
                 <div class="row">
                     <nav class="col-md-2 d-none d-md-block sidebar">
@@ -61,7 +61,7 @@
                                 </li>
                                 <li class="nav-item">
                                     <router-link class="nav-link" active-class="active" to="/lg/templates">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
+                                        <svg width="24" height="24" focusable="false" data-prefix="fas" data-icon="compact-disc" class="svg-inline--fa fa-compact-disc fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zM88 256H56c0-105.9 86.1-192 192-192v32c-88.2 0-160 71.8-160 160zm160 96c-53 0-96-43-96-96s43-96 96-96 96 43 96 96-43 96-96 96zm0-128c-17.7 0-32 14.3-32 32s14.3 32 32 32 32-14.3 32-32-14.3-32-32-32z"></path></svg>
                                         Templates
                                     </router-link>
                                 </li>
